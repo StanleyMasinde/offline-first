e9ac299e23b8504fc1d94008f8a632c5f4f97202
Stanley Masinde <stanmasinde@gmail.com>
2022-11-03 17:28:28 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+import './style.css'
+
+
+/**
+ * Register the service worker that controls this application
+ */
+if ('serviceWorker' in navigator) {
+  navigator
+    .serviceWorker
+    .register('/sw.js')
+    .then(registration => {
+      console.log(`Service worker registered for scope: ${registration.scope}`);
+    })
+    .catch(error => {
+      // Something went wrong!
+      console.log(error)
+    })
+} else {
+  // Service worker is not supported fail gracefully 
+}
+
+
+setupCounter(document.querySelector('#counter'))
